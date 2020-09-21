455e44c589b8e6d49ca0311abcc718e15d7e90ee
Shakira Rakibullah <srakibul@ucsd.edu>
2020-09-20 19:42:35 -0700

    Updated add

---
+++ README.md
@@ -0,0 +1,42 @@
+## Lab 1 - UI and Git Lab
+
+**NY Bestseller** is an android app that allows building a todo list and basic todo items management functionality including adding new items, editing and deleting an existing item.
+
+Submitted by: **Shakira R.**
+
+Time spent: **2** hours spent in total
+
+## User Stories
+
+The following **required** functionality is completed:
+
+* [x] Show more information about each best seller book including it's: ranking, image and description
+* [x] Add a 'Buy' button
+
+## Video Walkthrough
+
+Here's a walkthrough of implemented user stories:
+
+<img src='https://imgur.com/RYICyHC.gif' title='Video Walkthrough' width='' alt='Video Walkthrough' />
+
+GIF created with [LiceCap](http://www.cockos.com/licecap/).
+
+## Notes
+
+Describe any challenges encountered while building the app.
+
+## License
+
+    Copyright [2020] [Shakira R.]
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
---
+++ app/src/main/java/com/codepath/bestsellerlistapp/BestSellerBooksRecyclerViewAdapter.java
@@ -0,0 +1,97 @@
+package com.codepath.bestsellerlistapp;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.codepath.bestsellerlistapp.models.BestSellerBook;
+
+import java.text.BreakIterator;
+import java.util.List;
+
+/**
+ * {@link RecyclerView.Adapter} that can display a {@link BestSellerBook} and makes a call to the
+ * specified {@link OnListFragmentInteractionListener}.
+ */
+public class BestSellerBooksRecyclerViewAdapter extends RecyclerView.Adapter<BestSellerBooksRecyclerViewAdapter.BookViewHolder> {
+
+    private final List<BestSellerBook> books;
+    private final OnListFragmentInteractionListener mListener;
+
+    public BestSellerBooksRecyclerViewAdapter(List<BestSellerBook> items, OnListFragmentInteractionListener listener) {
+        books = items;
+        mListener = listener;
+    }
+
+    @Override
+    public BookViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.fragment_best_seller_book, parent, false);
+        return new BookViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(final BookViewHolder holder, int position) {
+        holder.mItem = books.get(position);
+        holder.mBookTitle.setText(books.get(position).title);
+        holder.mBookAuthor.setText(books.get(position).author);
+        holder.mRank.setText(String.valueOf(books.get(position).rank));
+        holder.mDescription.setText(books.get(position).description);
+        Glide.with(holder.mView).load(books.get(position).bookImageUrl).centerInside().into(holder.mBookImage);
+
+        holder.mView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (null != mListener) {
+                    // Notify the active callbacks interface (the activity, if the
+                    // fragment is attached to one) that an item has been selected.
+                    mListener.onItemClick(holder.mItem);
+                }
+            }
+        });
+        /*
+        BestSellerBook bestSellerBook = books.get(position);
+        Glide.with(holder.mView)
+                .load(bestSellerBook.bookImageUrl)
+                .centerInside()
+                .into(holder.mBookImage);
+
+         */
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return books.size();
+    }
+
+    public class BookViewHolder extends RecyclerView.ViewHolder {
+        public final View mView;
+        public final TextView mBookTitle;
+        public final TextView mBookAuthor;
+        public BestSellerBook mItem;
+        public final TextView mRank;
+        public final ImageView mBookImage;
+        public final TextView mDescription;
+
+        public BookViewHolder(View view) {
+            super(view);
+            mView = view;
+            mBookTitle = (TextView) view.findViewById(R.id.book_title);
+            mBookAuthor = (TextView) view.findViewById(R.id.book_author);
+            mRank = (TextView) view.findViewById(R.id.ranking);
+            mDescription = (TextView) view.findViewById(R.id.book_description);
+            mBookImage = (ImageView) view.findViewById(R.id.book_image);
+        }
+
+        @Override
+        public String toString() {
+            return mBookTitle.toString() + " '" + mBookAuthor.getText() + "'";
+        }
+    }
+}
